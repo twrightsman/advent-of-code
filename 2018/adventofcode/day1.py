14a4a7a3f7a1585ca0ca9c3296c6f41d00663667
Travis Wrightsman <traviswrightsman@gmail.com>
2018-12-09 22:20:09 -0500

    Add a solution for day 1, part 2

--- 2018/adventofcode/day1.py
+++ 2018/adventofcode/day1.py
@@ -1,9 +1,21 @@
-def solve_part1(input):
+from itertools import cycle
+
+
+def solve_part1(input_file):
     current_freq = 0
-    for line in input:
+    for line in input_file:
         current_freq += int(line.rstrip())
 
     return current_freq
 
-def solve_part2(input):
-    return False
+def solve_part2(input_file):
+    input_file = cycle(input_file)
+    current_freq = 0
+    # set is much faster for lookup operations than list
+    observed_freqs = {current_freq}
+    for line in input_file:
+        current_freq += int(line.rstrip())
+        if current_freq in observed_freqs:
+            return current_freq
+        else:
+            observed_freqs.add(current_freq)
--- 2018/tests.py
+++ 2018/tests.py
@@ -13,10 +13,30 @@ class TestDay1Part1(unittest.TestCase):
         self.assertEqual(self.solver(StringIO("+1\n+1\n+1\n")), 3)
 
     def test_mixed(self):
-        self.assertEqual(self.solver(StringIO("+1\n+1\n-2")), 0)
+        self.assertEqual(self.solver(StringIO("+1\n+1\n-2\n")), 0)
 
     def test_negative(self):
         self.assertEqual(self.solver(StringIO("-1\n-2\n-3\n")), -6)
+
+
+class TestDay1Part2(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.solver = staticmethod(getattr(import_module("adventofcode.day1"), 'solve_part2'))
+
+    def test_1(self):
+        self.assertEqual(self.solver(StringIO("+1\n-1\n")), 0)
+
+    def test_2(self):
+        self.assertEqual(self.solver(StringIO("+3\n+3\n+4\n-2\n-4\n")), 10)
+
+    def test_3(self):
+        self.assertEqual(self.solver(StringIO("-6\n+3\n+8\n+5\n-6\n")), 5)
+
+    def test_4(self):
+        self.assertEqual(self.solver(StringIO("+7\n+7\n-2\n-7\n-4\n")), 14)
+
 
 if __name__ == '__main__':
     unittest.main()
