f3a484c049be45d6fc76ef67b0de44def169fd7e
Travis Wrightsman <travis@wrightsman.org>
2020-12-03 11:19:24 -0500

    Improve the test code a bit

--- 2020/test/test_small.py
+++ 2020/test/test_small.py
@@ -5,19 +5,19 @@ from aoc.day2 import day2_part1, day2_part2
 
 
 class SmallTest(unittest.TestCase):
-    def test_day1_part1_small1(self):
-        report = "1721\n979\n366\n299\n675\n1456"
-        self.assertEqual(day1_part1(report), "514579")
 
-    def test_day1_part2_small1(self):
-        report = "1721\n979\n366\n299\n675\n1456"
-        self.assertEqual(day1_part2(report), "241861950")
+    day1_expense_report = "1721\n979\n366\n299\n675\n1456"
+    day2_password_database = "1-3 a: abcde\n1-3 b: cdefg\n2-9 c: ccccccccc"
 
-    def test_day2_part1_small1(self):
-        database = "1-3 a: abcde\n1-3 b: cdefg\n2-9 c: ccccccccc"
-        self.assertEqual(day2_part1(database), "2")
+    def test_day1_part1_small(self):
+        self.assertEqual(day1_part1(self.day1_expense_report), "514579")
 
-    def test_day2_part2_small1(self):
-        database = "1-3 a: abcde\n1-3 b: cdefg\n2-9 c: ccccccccc"
-        self.assertEqual(day2_part2(database), "1")
+    def test_day1_part2_small(self):
+        self.assertEqual(day1_part2(self.day1_expense_report), "241861950")
+
+    def test_day2_part1_small(self):
+        self.assertEqual(day2_part1(self.day2_password_database), "2")
+
+    def test_day2_part2_small(self):
+        self.assertEqual(day2_part2(self.day2_password_database), "1")
 
